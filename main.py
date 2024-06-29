a3c59209cb74b8f2987505640ee5f2aa898c99ed
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-29 12:49:10 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import scapy.all as scapy
 import time
-import os
 
-# Global variable to control sniffing
-sniffing = False
 output_file = "packet_sniffer_log.txt"
 
 def write_packet(packet):
@@ -16,22 +13,17 @@ def write_packet(packet):
         file.write("\n")
 
 def packet_callback(packet):
-    if sniffing:
-        write_packet(packet)
+    write_packet(packet)
 
 def start_sniffing(interface):
-    global sniffing
-    sniffing = True
     print(f"Started sniffing on {interface}")
     scapy.sniff(iface=interface, prn=packet_callback, store=False)
 
 def stop_sniffing():
-    global sniffing
-    sniffing = False
     print("Stopped sniffing")
 
 def choose_interface():
-    interfaces = os.listdir('/sys/class/net/')
+    interfaces = scapy.get_if_list()
     print("Available interfaces:")
     for i, interface in enumerate(interfaces):
         print(f"{i + 1}. {interface}")
@@ -39,8 +31,8 @@ def choose_interface():
     if 0 <= choice < len(interfaces):
         return interfaces[choice]
     else:
-        print("Invalid choice. Try again.")
-        return choose_interface()
+        print("Invalid choice. Exiting.")
+        exit()
 
 def main_menu():
     while True:
@@ -55,8 +47,6 @@ def main_menu():
         elif choice == '2':
             stop_sniffing()
         elif choice == '3':
-            if sniffing:
-                stop_sniffing()
             print("Exiting program.")
             break
         else:
